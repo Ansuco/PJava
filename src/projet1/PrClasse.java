45cd28db6116aded6b1b79a9475bca8e687b1cfd
Ansuco <omshiva.np@gmail.com>
2017-10-17 16:24:22 +0200

    Concatination of array and variables

--- src/projet1/PrClasse.java
+++ src/projet1/PrClasse.java
@@ -5,38 +5,11 @@ import java.util.Arrays;
 public class PrClasse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello ");
-		System.err.print("go go go");
-		System.out.print("bonjour ");
-		System.out.print("à tous ");
-		
-		// Use of variables
-		String nom = "salut ";
-		String prenom = "yo yo";
-		System.out.println(nom + prenom);
-		
-		//Use of Array	
-		String arrayHi[] = {"bonjour", "à tous"};
-		
-		System.out.println(arrayHi[0]);
-		System.out.println(arrayHi[1]);
-		System.out.println(arrayHi[0] + " " + arrayHi[1]);
-		
-		// Use of Loop for
-		for(int i=0; i<arrayHi.length; i++) {
-			System.out.println(arrayHi[i]);
-		}
-		
-		// Use of loop foreach
-		for(String test : arrayHi) {
-			System.out.println(test);
-		}
-		System.out.println(Arrays.asList(arrayHi));
-		System.out.println(Arrays.toString(arrayHi));
 
-
-
+		// use of array and variables and concatinate
+		String arrayToi[] = {"Tu as ", " ans"};
+		int age = 18;
+		System.out.println(arrayToi[0] + age + arrayToi[1]);
 
 	}
 
