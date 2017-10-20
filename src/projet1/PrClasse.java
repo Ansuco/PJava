0cf3c3b656f1dc5a417c981ebafe6d5a9ee4d034
Ansuco <omshiva.np@gmail.com>
2017-10-20 16:24:46 +0200

    boucle do while

--- src/projet1/PrClasse.java
+++ src/projet1/PrClasse.java
@@ -1,69 +1,59 @@
 package projet1;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class PrClasse {
 
 	public static void main(String[] args) {
 
-		// use of array and variables and concatinate
-		String arrayToi[] = {"Tu as ", " ans"};
-		int age = 18;
-		System.out.println(arrayToi[0] + age + arrayToi[1]);
-		
-		for(String you : arrayToi) {
-			if (you == " ans") {
-				System.out.print(age);	
-			}
-			System.out.println(you);
-		}
-		
-		// La condition ternaire
-		
-		Scanner sc = new Scanner(System.in);
-		age = sc.nextInt();
-		String reply = (3 < age) ? (age >= 18) ? "adult": "enfant" : "bébé";
-		System.out.println(reply);
-		
-	
-		
+		final int MONTH31 = 31;
+		final int MONTH30 = 30;
+
+		//Date Builder with multiple array
 		String dateBuilder[][] = {
-				{"janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "decembre"},
-				{"lundi","mardi","mecredi", "jeudi", "vendredi", "samedi","dimanche"}
+			{"janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "decembre"},
+			{"lundi","mardi","mecredi", "jeudi", "vendredi", "samedi","dimanche"}
 		};
 		
-		int nJours = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int month = 0;
+		String reponse = "";
 		
-			if (nJours <= 31) {
-				System.out.println(dateBuilder[0][0]);
-			}else if (nJours <= 31+30) {
-				System.out.println(dateBuilder[0][1]);
-			}else if (nJours <= 31+30+30) {
-				System.out.println(dateBuilder[0][2]);
-			}else if (nJours <= 31+30+30+30) {
-				System.out.println(dateBuilder[0][3]);
-			}else if (nJours <= 31+30+30+30+30) {
-				System.out.println(dateBuilder[0][4]);
-			}else if (nJours <= 31+30+30+30+30+30) {
-				System.out.println(dateBuilder[0][5]);
-			}else if (nJours <= 31+30+30+30+30+30+30) {
-				System.out.println(dateBuilder[0][6]);
-			}else if (nJours <= 31+30+30+30+30+30+30+30) {
-				System.out.println(dateBuilder[0][7]);
-			}else if (nJours <= 31+30+30+30+30+30+30+30+30) {
-				System.out.println(dateBuilder[0][8]);
-			}else if (nJours <= 31+30+30+30+30+30+30+30+30+30) {
-				System.out.println(dateBuilder[0][9]);
-			}else if (nJours <= 31+30+30+30+30+30+30+30+30+30+30) {
-				System.out.println(dateBuilder[0][10]);
-			}else if (nJours <= 31+30+30+30+30+30+30+30+30+30+30+30) {
-				System.out.println(dateBuilder[0][11]);
-			}else if (nJours <= 31+30+30+30+30+30+30+30+30+30+30+30+30) {
-				System.out.println(dateBuilder[0][12]);
-			}
-				
+		do {
+			System.out.print("Entrez quelque nombre [1-365] : ");
+			int nJours = sc.nextInt();
+			int cpt31 = 1, cpt30 = 1;
 			
+			if (nJours <= MONTH31 * cpt31) month = 0;
+			else if (nJours <= MONTH31 * cpt31 + MONTH30 * cpt30 - 1) month = 1;
+			else if (nJours <= MONTH31 * ++cpt31 + MONTH30 * cpt30 - 1) month = 2;
+			else if (nJours <= MONTH31 * cpt31 + MONTH30 * ++cpt30 - 1) month = 3;
+			else if (nJours <= MONTH31 * ++cpt31 + MONTH30 * cpt30 - 1) month = 4;
+			else if (nJours <= MONTH31 * cpt31 + MONTH30 * ++cpt30 - 1) month = 5;
+			else if (nJours <= MONTH31 * ++cpt31 + MONTH30 * cpt30 - 1) month = 6; // juillet
+			else if (nJours <= MONTH31 * ++cpt31 + MONTH30 * cpt30 - 1) month = 7; // aout
+			else if (nJours <= MONTH31 * cpt31 + MONTH30 * ++cpt30 - 1) month = 8;
+			else if (nJours <= MONTH31 * ++cpt31 + MONTH30 * cpt30 - 1) month = 9;
+			else if (nJours <= MONTH31 * cpt31 + MONTH30 * ++cpt30 - 1) month = 10;
+			else if (nJours <= MONTH31 * ++cpt31 + MONTH30 * cpt30 - 1) month = 11;
+			
+			System.out.println("Résultat : " + dateBuilder[0][month]);
+			
+			System.out.print("Voulez-vous recommencer ? [On] ");
+			sc.nextLine();
+			reponse = sc.nextLine();
+			
+		} while (!reponse.equals("n"));
 		
+		System.out.println("Aurevoir");
+		sc.close();
+
+		
+		
+//		System.out.println(dateBuilder[1][nSemaine- 1]);
+				
+		
+						
 	}
 }
